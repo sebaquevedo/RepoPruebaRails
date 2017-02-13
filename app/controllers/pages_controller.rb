8a16a2125db815f26aedad391b2602d01b2d0cbb
sebastian quevedo <sebaqd91@gmail.com>
2017-02-13 17:40:24 -0300

    cambios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def x
   end
+
+    def save_user
+  		User.create(name: params[:a],age: params[:b],mail: params[:c])
+  		redirect_to pages_x_path, notice: "El usuario #{params[:a]} ha sido guardado"
+  	end
+
+  def see_users
+  	@users = User.all
+  	@hola = "Esta es la pagina see_users"
+  	asdas
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#x'
   get 'pages/x'
+  post 'pages/save_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
